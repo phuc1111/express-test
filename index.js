48379817f7b476a8cf76a2ef2be6920222f76562
thien <phuc.dam@student.passerellesnumeriques.org>
2020-04-20 22:58:36 +0700

    fix port

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 var mongoose = require('mongoose')
 mongoose.connect(process.env.MONGO_URL);
