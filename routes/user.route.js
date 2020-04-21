87eb6e1a8764e27996666b38b6a246e9611585d9
thien <phuc.dam@student.passerellesnumeriques.org>
2020-04-21 13:00:06 +0700

    edit mongoDB atlat

--- routes/user.route.js
+++ routes/user.route.js
@@ -6,7 +6,11 @@ const shortid = require('shortid');
 var authMiddleware = require('../middleware/auth.middleware')
 var userValidate = require('../validate/user.validate')
     // router.get('/:id', controller.get)
-router.get('/', controller.index)
+function checkUserAuth(req, res, next) {
+    if (req.session.user) return next();
+    return next(new NotAuthorizedError());
+}
+router.get('/', checkUserAuth, controller.index)
 router.get('/:id', controller.detail)
 router.get('/create', controller.create)
 router.post('/create', controller.postCreate)
